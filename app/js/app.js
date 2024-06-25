bcc15c2176266ba2fe2306698b0d9a2b69d8d8cf
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-06-25 20:17:24 +0300

    add input tel countries + form validation

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,31 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import intlTelInput from 'intl-tel-input';
 
 document.addEventListener('DOMContentLoaded', () => {
-
 	// Custom JS
+	const input = document.querySelector('#phone');
 
-})
+	intlTelInput(input, {
+		initialCountry: 'ua',
+		strictMode: true,
+		separateDialCode: true,
+		utilsScript: './utils.js',
+	});
+
+	const form = document.getElementById('form');
+
+	form.addEventListener('submit', (event) => {
+		event.preventDefault(); // Prevent default form submission
+
+		const name = document.getElementById('name').value;
+		const phone = document.getElementById('phone').value;
+		const email = document.getElementById('email').value;
+
+		const jsonData = {
+			name: name,
+			phone: phone,
+			email: email,
+		};
+
+		confirm('Form Data: ' + JSON.stringify(jsonData));
+	});
+});
